99217c18733f8982a855a9733fbed71455254ee3
kugaga <kuuga20010912@gmail.com>
2023-01-01 21:07:15 +0900

    ポートgit初

---
+++ port.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>ポートフォリオ</title>
+  <link rel="stylesheet" type="text/css" href="port.css">
+</head>
+<header>
+
+  <div class="container">
+  <h1>脇坂空雅</h1>
+  <div>
+</header>  
+<body>
+<div class="backgruoud">  
+  <div class="menu-wrapper container">
+    <h1></h1>
+    <div class="explanation">
+    <p>現在、名古屋大学理学部数理学科B2です。</p>
+    <p>10月から1月までに学生運営のキャリア支援カフェで店長をしておりました。</p>
+    <p>そして、去年の8月からプログラミングを学習を始めました。</p>
+    
+    <div>
+  <div>  
+
+<div>  
+</body>
+</html>
