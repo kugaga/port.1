919baf5dc18a4e5a25bf3c40436a117d4b40dd7c
kugaga <kuuga20010912@gmail.com>
2023-01-03 19:13:33 +0900

    ヘッダー追加＆背景変更

--- port.php
+++ port.php
@@ -5,48 +5,38 @@
   <title>ポートフォリオ</title>
   <link rel="stylesheet" type="text/css" href="port.css">
 </head>
+<div class="backgruoud">  
 <header>
 
-  <div class="container">    
     <div class="header-left">
-    <div>
+      
+    </div>
     <div class="header-right">
     
       <div class="one-header">
-      <div class="Message">
-        <p>Message</p>
-      <div>
-      <div>
+          <p class="Message">Message</p>
+      </div>
       <div class="one-header">    
-      <div class="About">
-        <p>About</p>
-      <div>  
-      <div>
-      <div class="one-header">      
-      <div class="Works">
-        <p>Works</p>
-      <div>
-      <div>    
+          <p class="About">About</p>  
+      </div>
+      <div class="one-header">              
+          <p class="Works">Works</p>
+      </div>
       <div class="one-header">     
-      <div class="Contact">
-        <p>Contact</p>
-      <div>
-      <div>    
-    <div>      
-  <div>
+          <p class="Contact">Contact</p>
+      </div>
+    </div>      
 </header>  
 <body>
-<div class="backgruoud">  
-  <div class="menu-wrapper container">
-    <h1></h1>
+  <div class="menu-wrapper">
     <div class="explanation">
     <p>現在、名古屋大学理学部数理学科B2です。</p>
     <p>10月から1月までに学生運営のキャリア支援カフェで店長をしておりました。</p>
     <p>そして、去年の8月からプログラミングを学習を始めました。</p>
     
-    <div>
-  <div>  
+    </div>
+</div>  
 
-<div>  
+</div>  
 </body>
 </html>
