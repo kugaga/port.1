e9247106b653c00ee8228b312c01ef68aa2e2c32
kugaga <kuuga20010912@gmail.com>
2023-01-06 23:59:28 +0900

    背景変更

--- contact.php
+++ contact.php
@@ -1,3 +1,26 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $address=$_POST['address'];
+    $col=$_POST['col'];
+    try{
+        $dsn = 'mysql:dbname=porttry2;host=localhost';
+        $user = 'root';
+        $password ='nagoya60809';
+        $db= new PDO($dsn, $user, $password);
+        $sql = 'insert into users(name,address,col) values(?, ?, ?)';
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array($name, $address, $col));
+        $stmt = null;
+        $db =null;
+    }catch(PDOExcept $e){
+        echo $e->getMessage();
+        exit;
+    }
+}       
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,26 +28,25 @@
   <title>ポートフォリオ</title>
   <link rel="stylesheet" type="text/css" href="port.css">
 </head>
-<div class="backgruoud">  
+<div class="backgruoud"> 
 <header>
 
     <div class="header-left">
       
     </div>
     <div class="header-right">
-    
-      <div class="one-header">
+        <div class="one-header">
           <a href="port.php" class="Message">Message</a>
-      </div>
-      <div class="one-header">    
-          <p class="About">About</p>  
-      </div>
-      <div class="one-header">              
-          <p class="Works">Works</p>
-      </div>
-      <div class="one-header">     
-          <p class="Contact">Contact</p>
-      </div>
+        </div>
+        <div class="one-header">    
+          <a class="About">About</a>  
+        </div>
+        <div class="one-header">              
+          <a class="Works">Works</a>
+        </div>
+        <div class="one-header">     
+          <a href="contact.php" class="Contact">Contact</a>
+        </div>
     </div>      
 </header>  
 <body>
@@ -33,16 +55,16 @@
         <div class="Contitle">
            <p>Contact</p>
         </div>        
-        <div class="-form">    
-            <form action="portsent.php" method="post">
+        <div class="in-form">    
+            <form action="" method="POST">
                 <p>Name</p>
             <input type="text" name="name">
-                <p>Email<p>
-            <input type="text" name="email">
+                <p>Email</p>
+            <input type="text" name="address">
                 <p>Message</p>
-            <textarea name="body"></textarea>
+            <textarea name="col"></textarea>
             
-            <input type="submit" value="Send">
+            <input type="submit" name="submit" value="Send">
             </form>
         </div>
     </div>
