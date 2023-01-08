3ea80b94dc8cd475e37cd5098f3ec6c869509080
kugaga <kuuga20010912@gmail.com>
2023-01-09 02:51:35 +0900

    about更新

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
     <div class="header-right">
     
       <div class="one-header">
-          <a href="port.php" class="Message">Message</a>
+          <a class="Message">Message</a>
       </div>
       <div class="one-header">    
           <a href="about.php" class="About">About</a>  
@@ -29,13 +29,21 @@
     </div>      
 </header>  
 <body>
-  <div class="menu-wrapper">
-    <div class="explanation">
-    <p>名古屋大学理学部数理学科B2です。</p>
-    <p>10月から1月までに学生運営のキャリア支援カフェで店長をしておりました。</p>
-    <p>そして、去年の8月からプログラミングを学習を始めました。</p>
-    
+    <div class="Wrapper-About">
+        <div class="Entire-About">
+            <div class="About-phote">
+                <img src="mephote.jpg">
+            </div>
+            <div class="AboutExplation">
+                <p>現在、名古屋大学理学部数理学科二年生在籍</p>
+                <p>4月から9月、10月から12月までにキャリア支援カフェで店長代理・店長を経験<p>
+                <p>去年の8月からプログラミング学習を開始</p>
+                <p>WordPress,JavaScript,PHP勉強中</p>
+             </div>
+        </div>
     </div>
+    
+
 </div>  
 
 </div>  
