f0e6a1bc7d471e4327ef95a838953664f9de13fc
kugaga <kuuga20010912@gmail.com>
2023-01-09 07:52:37 +0900

    about仮完成

--- about.php
+++ about.php
@@ -30,21 +30,40 @@
 </header>  
 <body>
     <div class="Wrapper-About">
-        <div class="Entire-About">
-            <div class="About-phote">
-                <img src="mephote.jpg">
+        <div class="btn1-Wrapper">
+            <div class="AboutExplanation">
+                <h3>脇坂空雅 | Kuga Wakisaka</h3>
+                <p>愛知県江南市生まれ。西春高校卒業し、1年浪人後、数学に感心を持ち名古屋大学理学部入学、翌年数理学科に所属。1年生のときにキャリア支援カフェに所属し、2年生の4月から9月、10月から1月まで店長代理、店長をそれぞれ経験。</p>
+                <p>2年生の8月、エンジニアに興味を持ちプログラミングを独学で学ぶ。現在Javascript、PHP、WordPressを学習中。</p>
+
             </div>
-            <div class="AboutExplation">
-                <p>現在、名古屋大学理学部数理学科二年生在籍</p>
-                <p>4月から9月、10月から12月までにキャリア支援カフェで店長代理・店長を経験<p>
-                <p>去年の8月からプログラミング学習を開始</p>
-                <p>WordPress,JavaScript,PHP勉強中</p>
-             </div>
+            <div class="AboutCareer">
+                <p>2017.04  西春高校入学</p>
+                <p>2020.03  西春高校卒業</p>
+                <p>2021.04  名古屋大学理学部入学</p>
+                <p>2022.04  名古屋大学理学部数理学科所属</p>
+                <p>2022.08  プログラミング学習開始</p>
+
+            </div>
         </div>
     </div>
+    <div class="btn2-Wrapper">
+        <div class="AboutPhote">
+            <img src="suno.jpg">
+        </div>
+        <div class="AboutProfeel">
+            <p>趣味：サウナ、ジョギング、スノボ</p>
+            <p>長所：分析力・行動力</p>
+            <p>短所：優柔不断</p>
+            <p>将来行きたい場所：ペルーのマチュピチュ</p>
+            <p>目標：個人でサイトの業務委託できるようになる</p>
+        </div>
+    </div>
+</div>
     
 
-</div>  
+</div>
+<div class="Background2">
 
 </div>  
 </body>
